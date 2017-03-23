0ac00714c3395df302e83cb5c9b7e78fa54557a8
casperjureskog <lcasper@mac.com>
2017-03-23 10:15:12 +0100

    add js

--- cooper_api/app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
- # Include default devise modules.
- devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-         # :confirmable, :omniauthable
- include DeviseTokenAuth::Concerns::User
- has_many :performance_data, class_name: 'PerformanceData'
-end
--- cooper_api/spec/factories/performance_data.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :performance_datum, class: 'PerformanceData' do
-    user nil
-    data ""
-  end
-end
